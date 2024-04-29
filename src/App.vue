02b9312df73ff2c9126fcbc35201d143dc248b9c
Junior Paula <luke.junnior@icloud.com>
2024-04-29 08:11:55 -0300

    feat: create Header component

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,15 @@
 <template>
-  <header class="bg-gray-800 py-4">
-    <div class="container mx-auto flex justify-between items-center px-4">
-        <!-- Logo -->
-        <a href="#" class="text-white font-bold text-xl">Logo</a>
-        <!-- Navbar -->
-        <nav class="hidden md:block">
-            <ul class="flex space-x-4">
-                <li><a href="#" class="text-white">Home</a></li>
-                <li><a href="#" class="text-white">Sobre</a></li>
-                <li><a href="#" class="text-white">Serviços</a></li>
-                <li><a href="#" class="text-white">Contato</a></li>
-            </ul>
-        </nav>
-        <div class="flex space-x-4">
-            <p class="text-white">nome do usuário</p>
-            <a href="#" class="text-red-500">Sair</a>
-        </div>
-    </div>
-  </header>
+ <Header /> 
 </template>
 
 <script>
+import Header from "./components/Header.vue";
+
 export default {
+  name: "App",
+  components: {
+    Header,
+  },
  data() {
   return {
    title: "Hello World!",
---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header class="bg-gray-800 py-4">
+    <div class="container mx-auto flex justify-between items-center px-4">
+        <!-- Logo -->
+        <a href="#" class="text-white font-bold text-xl">Logo</a>
+        <!-- Navbar -->
+        <nav class="hidden md:block">
+            <ul class="flex space-x-4">
+                <li><a href="/prodcuts" class="text-white">Produtos</a></li>
+                <li><a href="/users" class="text-white">Usuários</a></li>
+            </ul>
+        </nav>
+        <div class="flex space-x-4">
+            <p class="text-white">nome do usuário</p>
+            <a href="#" class="text-red-500">Sair</a>
+        </div>
+    </div>
+  </header>
+</template>
+
+<script>
+
+export default {
+  name: "Header",
+}
+</script>
