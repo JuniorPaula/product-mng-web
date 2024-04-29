8d9519aacac03ab8495e7e956b04c85d9aaa482c
Junior Paula <luke.junnior@icloud.com>
2024-04-29 08:19:19 -0300

    feat: create Footer component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
- <Header /> 
+ <Header />
+  <div class="container mx-auto p-4">
+    <h1 class="text-2xl font-bold text-center">{{ title }}</h1>
+    <p class="text-center">Welcome to your Vue.js application</p>
+  </div>
+  <Footer />
 </template>
 
 <script>
 import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
   name: "App",
   components: {
     Header,
+    Footer,
   },
  data() {
   return {
---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="bg-gray-800 py-4">
+    <div class="container mx-auto flex justify-between items-center px-4">
+        <p class="text-white">Todos os direitos reservados</p>
+        <p class="text-white">&copy {{ new Date().getFullYear() }}</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+}
+</script>
