5df1209d9e4452f0ee0a9fcb3ce6eefd00a83b2c
Junior Paula <luke.junnior@icloud.com>
2024-04-30 11:32:37 -0300

    feat: implements Security method to verify token

--- src/request/security.js
+++ src/request/security.js
@@ -1,4 +1,5 @@
 import { store } from "../store";
+import { configs } from "../configs";
 import router from "../routes";
 
 export let Security = {
@@ -19,5 +20,35 @@ export let Security = {
       headers: headers,
       body: JSON.stringify(payload),
     };
+  },
+
+  checkToken: async  function() {
+    if (store.token !== "") {
+      const payload = { token: store.token};
+      const headers = new Headers();
+      headers.append("Content-Type", "application/json");
+
+      let options = {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(payload),
+      };
+
+      const response = await fetch(`${configs.API_URL}/verify-token`, options);
+      const data = await response.json();
+      if (data.error) {
+        store.token = "";
+        store.user = {};
+        store.isLogged = false;
+
+        // delete cookie
+        document.cookie = "_site=; path=/; "
+        + "SameSite=Srict; Secure; expires=Thu, 01 Jan 1970 00:00:00 GTM;"
+        
+        return false;
+      } else {
+        return true;
+      }
+    }
   }
 }
