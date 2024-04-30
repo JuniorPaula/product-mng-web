e38732b862fa2ec9e111e56234a5e8bf72e45333
Junior Paula <luke.junnior@icloud.com>
2024-04-30 19:33:14 -0300

    feat: create component to edit one product

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -20,17 +20,16 @@
           <td class="border px-4 py-2">{{ p.id }}</td>
           <td class="border px-4 py-2">{{ p.name }}</td>
           <td class="border px-4 py-2">R$ {{ p.price }}</td>
-          <td class="border px-4 py-2">
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold p-2 mx-2 rounded">
+          <td class="border px-4 py-2 flex">
+            <router-link :to="`/product/${p.id}`" class="bg-blue-500 hover:bg-blue-700 text-white font-bold p-2 mx-2">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                 stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round"
                   d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
               </svg>
-
-            </button>
+            </router-link>
             <button @click="() => openModal('modalDelete', p.id)"
-              class="bg-red-500 hover:bg-red-700 text-white font-bold p-2 rounded">
+              class="bg-red-500 hover:bg-red-700 text-white font-bold p-2">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                 stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round"
---
+++ src/components/Product.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="text-center">
+    <h2 class="text-3xl font-medium mt-8 text-gray-700">Atualizar Produto</h2>
+  </div>
+  <div class="max-w-md mx-auto mt-8">
+    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <div class="mb-4">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
+          Nome:
+        </label>
+        <input
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="name" type="text" placeholder="Nome" v-model="this.product.name">
+      </div>
+      <div class="mb-6">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="price">
+          Preço:
+        </label>
+        <input
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="price" type="text" placeholder="Preço" v-model="this.product.price">
+      </div>
+      <div class="flex items-center justify-between">
+        <button @click.prevent="update"
+          class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline"
+          type="button">
+          Atualizar
+        </button>
+        <router-link to="/"
+          class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline"
+          type="button">
+          Voltar
+        </router-link>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import { configs } from '@/configs';
+import { Security } from '@/request/security';
+
+export default {
+  name: "Product",
+  data() {
+    return {
+      product: {
+        name: "",
+        price: ""
+      }
+    };
+  },
+  beforeMount() {
+    this.getProduct();
+  },
+  methods: {
+    getProduct() {
+      fetch(`${configs.API_URL}/products/${this.$route.params.id}`, Security.requestOptions(null))
+        .then((response) => response.json())
+        .then(({ error, message, data }) => {
+          if (error) {
+            console.error(message);
+            return;
+          }
+          this.product.name = data.name;
+          this.product.price = data.price;
+        });
+    },
+  }
+}
+</script>
